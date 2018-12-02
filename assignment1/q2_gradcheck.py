9fa0e22c275e5b88a1342d4f6656196f96ba342b
Sunghee Jung <hash2430@gmail.com>
2018-12-02 15:55:54 +0900

    multi-dimension에 대해 gradient check하도록 기능 추가 numerical grad를 x의 각각 dimension으로 x를 움직여 구한 것과 lambda expression으로 정의한 미분식이 주는 값을 비교, gradient의 절대값으로 오차를 normalize한 후 그 크기가 1e-4보다 작을 경우 gradient가 valid한 것으로 판단한다. 그렇지 않다면 assert
    구현상의 유의사항:
    - numpy.nditer()의 사용 방법
    - random.getstate(), random.setstate()의 사용
    - readwrite nditer op_flags에 의해 변경된 original array를
    테스트 후 복원하는 것
    - nditer 사용 후 it.iternext()를 호출해 다음 인덱스로 넘기는 것

--- assignment1/q2_gradcheck.py
+++ assignment1/q2_gradcheck.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 # First implement a gradient checker by filling in the following functions
 def gradcheck_naive(f, x):
     """ Gradient check for a function f.
@@ -8,11 +9,42 @@ def gradcheck_naive(f, x):
          cost and its gradients
     x -- the point (numpy array) to check the gradient at
     """
-    val, gt_grad = f(x)
-    prev_x = x - 1e-5
-    prev_val,_ = f(prev_x)
-    grad = (val - prev_val)/(x-prev_x)
-    assert(np.abs(grad-gt_grad) >= 1e-5)
+    rndstate = random.getstate()
+    random.setstate(rndstate)
+    fx, grad = f(x) # Evaluate function value at original point
+    h = 1e-4        # Do not change this!
+
+    # Iterate over all indexes in x
+    # x의 각각의 dimension에 대한 각각의 gradient가 numerical gradient와 일치하는지 dimension 별로 체크해봐야함
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    # x[ix] is writable (by its op_flags) thus it needs to be restored at the end of each turn
+    while not it.finished:
+        ix = it.multi_index
+
+        # Try modifying x[ix] with h defined above to compute
+        # numerical gradients. Make sure you call random.setstate(rndstate)
+        # before calling f(x) each time. This will make it possible
+        # to test cost functions with built in randomness later.
+
+        ### YOUR CODE HERE:
+        """
+        specify ix index for the movement of x in only one dimension and not the others
+        x is changed 
+        """
+        x[ix] = x[ix] + h
+        random.setstate(rndstate)
+        right_fx,_ = f(x)
+        x[ix] = x[ix] - 2 * h
+        random.setstate(rndstate)
+        left_fx, _ = f(x)
+        x[ix] = x[ix] + h
+        numeric_grad = (right_fx - left_fx)/(2*h)
+        # normalization_term: gradient 자체의 크기에 따라 허용되는 gradient 오차 범위도 달라지기 대문에 normalization 필요
+        normalization_term = max(1, numeric_grad, grad[ix])
+        grad_error = np.abs(numeric_grad - grad[ix]) / normalization_term
+        assert(grad_error < h)
+        it.iternext()
+    print("Gradient check passed!")
 
 def sanity_check():
     """
